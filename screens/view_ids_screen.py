eb333e9425eec02e62b12161447bb9847edffbc9
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-09 01:32:19 +0530

    Add buttons fro view_ids_screen

--- screens/view_ids_screen.py
+++ screens/view_ids_screen.py
@@ -2,6 +2,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
 
 class ViewIDsScreen(Screen):
@@ -9,16 +10,25 @@ class ViewIDsScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        layout = BoxLayout(orientation='vertical', spacing=20, padding=40)
+        main_layout = BoxLayout(orientation='vertical', spacing=20, padding=40)
 
-        layout.add_widget(Label(text="View IDs Screen"))
+        # Scroll area for IDs
+        self.scroll = ScrollView
+        self.ids_layout =   BoxLayout(orientation= 'vertical', size_hint_y=None)
+        self.ids_layout.bind(minimum_height=self.ids_layout.setter('height'))
 
-        back_btn = Button(text="Back to Home")
+        self.scroll.add_widget(self.ids_layout)
+
+        # Load Button
+        load_btn = Button(text="Load Saved IDs", size_hint=(1, 0.2))
+        load_btn.bind(on_press=self.load_ids)
+
+        # Back Button
+        back_btn = Button(text="Back to Home", size_hint=(1, 0.2))
         back_btn.bind(on_press=self.go_home)
 
-        layout.add_widget(back_btn)
+        main_layout.add_widget(load_btn)
+        main_layout.add_widget(self.scroll)
+        main_layout.add_widget(back_btn)
 
-        self.add_widget(layout)
-
-    def go_home(self, instance):
-        self.manager.current = "home"
+        self.add_widget(main_layout)
